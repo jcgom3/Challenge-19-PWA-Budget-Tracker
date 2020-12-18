a0b5ef9cf6b2fd5428645c283c5fe9d3b8736875
Juan  Gomez <jcgom3@gmail.com>
2020-12-18 07:00:25 -0500

    idk

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,28 @@
+//when user has accessed app and then tries to access again offline...cached files will be stored in users' device and user will still have access
+const FILES_TO_CACHE = [
+  "/",
+  "./index.html",
+  "./manifest.json",
+  "./js/index.js",
+  "./js/idb.js",
+  "./css/style.css",
+  "./service-worker.js",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png"
+];
+
 const APP_PREFIX = 'BudgetTrackerPWA-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const DATA_CACHE_NAME = 'DATA'+ APP_PREFIX + VERSION
 
 
-//when user has accessed app and then tries to access again offline...cached files will be stored in users' device and user will still have access
-const FILES_TO_CACHE = [
-    "/",
-    "./index.html",
-    "./manifest.json",
-    "./js/index.js",
-    "./js/idb.js",
-    "./css/style.css",
-    "./service-worker.js",
-    "/icons/icon-128x128.png",
-    "/icons/icon-144x144.png",
-    "/icons/icon-384x384.png",
-    "/icons/icon-512x512.png",
-    "/icons/icon-152x152.png",
-    "/icons/icon-192x192.png",
-    "/icons/icon-72x72.png",
-    "/icons/icon-96x96.png"
-  ];
 
   // Install the service worker
 self.addEventListener('install', function(evt) {
