25fa5bb509c0c95867a8d4473a4b72aefeefdf89
Juan  Gomez <jcgom3@gmail.com>
2020-12-18 20:28:02 -0500

    fixing bug 333344

--- public/js/index.js
+++ public/js/index.js
@@ -13,14 +13,14 @@ fetch("/api/transaction")
     populateTable();
     populateChart();
   });
-function populateTotal() {
-  // reduce transaction amounts to a single total value
-  let total = transactions.reduce((total, t) => {
-    return total + parseInt(t.value);
-  }, 0);
-  let totalEl = document.querySelector("#total");
-  totalEl.textContent = total;
-}
+// function populateTotal() {
+//   // reduce transaction amounts to a single total value
+//   let total = transactions.reduce((total, t) => {
+//     return total + parseInt(t.value);
+//   }, 0);
+//   let totalEl = document.querySelector("#total");
+//   totalEl.textContent = total;
+// }
 function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
