3614baf6877557b1c929260555d2d7c22d4efacc
Juan  Gomez <jcgom3@gmail.com>
2020-12-18 20:55:37 -0500

    fix bugs 999

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,18 @@ fetch("/api/transaction")
     // save db data on global variable
     console.log(data);
     transactions = data;
-    // populateTotal();
+    populateTotal();
     populateTable();
     populateChart();
   });
-// function populateTotal() {
-//   // reduce transaction amounts to a single total value
-//   let total = transactions.reduce((total, t) => {
-//     return total + parseInt(t.value);
-//   }, 0);
-//   let totalEl = document.querySelector("#total");
-//   totalEl.textContent = total;
-// }
+function populateTotal() {
+  // reduce transaction amounts to a single total value
+  let total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
+  let totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
 function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
@@ -96,7 +96,7 @@ function sendTransaction(isAdding) {
   // re-run logic to populate ui with new record
   populateChart();
   populateTable();
-  // populateTotal();
+  populateTotal();
   
   
   
